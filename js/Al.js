bdcb0ecbfbca16c003e78d93f1fff9657a89fe7c
mohammad sakib <sakibmohammad7679@gmail.com>
2023-03-02 21:13:43 +0600

    card added and all data title added this card

--- js/Al.js
+++ js/Al.js
@@ -6,8 +6,27 @@ const loadAiData = () => {
 }
 
 const displayAiData = (datas) => {
+    const detailsContainer = document.getElementById('details-container');
     datas.forEach(data => {
-        //console.log(data);
+        console.log(data);
+        const detailsDiv = document.createElement('div');
+        detailsDiv.classList.add('col');
+        detailsDiv.innerHTML = `
+        <div class="card h-100">
+                <img src="${data ? data.image : 'No images'}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Features</h5>
+                  <p class="card-text">1. ${data ? data.features[0] : 'No Features Found'}</p>
+                  <p class="card-text">2. ${data ? data.features[1] : 'No Features Found'}</p>
+                  <p class="card-text">3. ${data ? data.features[2] : 'No Features Found'}</p>
+                </div>
+                <div class="card-footer">
+                  <h5 class="footer-title">${data ? data.name : 'No Name Found'}</h5>
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+        `
+        detailsContainer.appendChild(detailsDiv);
     })
 }
 
