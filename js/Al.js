5f7ca3526dfc6a5d07c847a8e9d55ac092ca9dd7
mohammad sakib <sakibmohammad7679@gmail.com>
2023-03-04 01:54:25 +0600

    cross button are added

--- js/Al.js
+++ js/Al.js
@@ -82,10 +82,11 @@ const showAiDetails = (data) => {
     modalContainers.innerHTML = '';
     const modalDiv = document.createElement('div');
     modalDiv.innerHTML = `
+    
     <div class="card w-fluid bg-warning-subtle">
         <div class="card-body">
+            
             <h5 class="card-text">${data? data.description : 'Description not found'}</h5>
-
             <div class="d-flex justify-content-between text-center gap-1 fw-bold">
                 <div class="bg-white p-2 rounded-3">
                     <p>${data.pricing ? data.pricing[0].price : 'No price'}<br>${data.pricing ? data.pricing[0].plan : 'No plan'}</p>
@@ -121,15 +122,17 @@ const showAiDetails = (data) => {
     </div>
     `
     modalContainers.appendChild(modalDiv);  
-
     const modalImg = document.getElementById('modal-img');
     modalImg.innerHTML = '';
     const modalImgContainer = document.createElement('div');
     modalImgContainer.innerHTML = `
     
     <div class="card">
+    <div id="cross">
+    <i class="fs-1 text-danger fa-sharp fa-solid fa-circle-xmark" data-bs-dismiss="modal"></i>
+    </div>
         <div id="badges">
-        <span class="badge fs-5 rounded-pill text-bg-danger">${data.accuracy ? data.accuracy.score*100 : 'no data found'}% accuracy</span>
+        <span class="badge fs-4 fs-md-5 p-2 p-md-3 rounded-pill text-bg-danger">${data.accuracy ? data.accuracy.score*100 : "data not found"}% accuracy</span>
         </div>
             <img src="${data.image_link ? data.image_link[0]: 'image not found'}" class="card-img-top img-fluid" alt="...">
         <div class="card-body">
@@ -145,39 +148,7 @@ const showAiDetails = (data) => {
 document.getElementById('show-more').addEventListener('click', function(){
     loadAiData();
 })
+
 loadAiData(6);
 
 //style="width: 18rem;
-
-//const modalTitle = document.getElementById('modal-description');
-    //modalTitle.innerText = data.description
-// const features = document.getElementById('Featurs');
-    // features.innerText = data.features ? data.features[1].feature_name : 'No data found';
-
-    // const features1 = document.getElementById('Featurs1');
-    // features1.innerText = data.features ? data.features[2].feature_name : 'No data found';
-
-    // const features2 = document.getElementById('Featurs2');
-    // features2.innerText = data.features ? data.features[3].feature_name : 'No data found';
-
-    // const priceContainer = document.getElementById('price1');
-    // priceContainer.innerHTML = `
-    // <p>${data.pricing? data.pricing[0].price : 'No data found'}<br>${data.pricing?data.pricing[0].plan : 'No data found'}</p>
-    // `
-    // const priceContainer1 = document.getElementById('price2');
-    // priceContainer1.innerHTML = `
-    // <p>${data.pricing?data.pricing[1].price:'No data found'}<br>${data.pricing?data.pricing[1].plan:'No data found'}</p>
-    // `
-    // const priceContainer2 = document.getElementById('contact');
-    // priceContainer2.innerHTML = `
-    // <p>${data.pricing?data.pricing[2].price: 'No data found'}<br>${data.pricing?data.pricing[2].plan: 'No data found'}</p>
-    // `
-
-    // const Integration = document.getElementById('Integration');
-    // Integration.innerText =data ? data.integrations[0]: 'No data found';
-
-    // const Integration1 = document.getElementById('Integration1');
-    // Integration1.innerText = data ? data.integrations[1]: 'No data found';
-
-    // const Integration2 = document.getElementById('Integration2');
-    // Integration2.innerText =data ? data.integrations[2]: 'No data found';
