fe831e037eaff2f11b8df6a008938687e7c0c26e
mohammad sakib <sakibmohammad7679@gmail.com>
2023-03-03 20:03:00 +0600

    modal details are dynamic

--- js/Al.js
+++ js/Al.js
@@ -75,42 +75,52 @@ const loadAiDetails= async (id) =>{
     showAiDetails(data.data); 
 }
 
+
 const showAiDetails = (data) => {
     console.log(data);
-    const modalTitle = document.getElementById('modal-description');
-    modalTitle.innerText = data.description
-    
+    const modalContainers = document.getElementById('modal-details');
+    modalContainers.innerHTML = '';
+    const modalDiv = document.createElement('div');
+    modalDiv.innerHTML = `
+    <div class="card w-fluid bg-warning-subtle">
+        <div class="card-body">
+            <h5 class="card-text">${data? data.description : 'Description not found'}</h5>
 
-    const features = document.getElementById('Featurs');
-    features.innerText = data.features ? data.features[1].feature_name : 'No data found';
-
-    const features1 = document.getElementById('Featurs1');
-    features1.innerText = data.features ? data.features[2].feature_name : 'No data found';
-
-    const features2 = document.getElementById('Featurs2');
-    features2.innerText = data.features ? data.features[3].feature_name : 'No data found';
-
-    const priceContainer = document.getElementById('price1');
-    priceContainer.innerHTML = `
-    <p>${data.pricing? data.pricing[0].price : 'No data found'}<br>${data.pricing?data.pricing[0].plan : 'No data found'}</p>
+            <div class="d-flex justify-content-between text-center gap-1 fw-bold">
+                <div class="bg-white p-2 rounded-3">
+                    <p>${data.pricing ? data.pricing[0].price : 'No price'}<br>${data.pricing ? data.pricing[0].plan : 'No plan'}</p>
+                </div>
+                    <div class="bg-white p-2 rounded-3">
+                    <p>${data.pricing ? data.pricing[1].price : 'No price'}<br>${data.pricing ? data.pricing[1].plan : 'No plan'}</p>
+                </div>
+                <div class="bg-white p-2 rounded-3">
+                    <p>${data.pricing ? data.pricing[2].price : 'No price'}<br>${data.pricing ? data.pricing[2].plan : 'No plan'}</p>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between">
+                <div>
+                    <h5>Features</h5>
+                    <ul>
+                        <li>${data.features? data.features[1].feature_name : 'No Features'}</li>
+                        <li>${data.features? data.features[2].feature_name : 'No Features'}</li>
+                        <li>${data.features? data.features[3].feature_name : 'No Features'}</li>
+                    </ul>
+                </div
+                <div>
+                    <div>
+                    <h5>integrations</h5>
+                    <ul>
+                        <li>${data ? data.integrations[0] : 'No integrations'}</li>
+                        <li>${data ? data.integrations[1] : 'No integrations'}</li>
+                        <li>${data ? data.integrations[2] : 'No integrations'}</li>
+                    </ul>
+                    </div>
+                </div
+            </div>
+        </div>
+    </div>
     `
-    const priceContainer1 = document.getElementById('price2');
-    priceContainer1.innerHTML = `
-    <p>${data.pricing?data.pricing[1].price:'No data found'}<br>${data.pricing?data.pricing[1].plan:'No data found'}</p>
-    `
-    const priceContainer2 = document.getElementById('contact');
-    priceContainer2.innerHTML = `
-    <p>${data.pricing?data.pricing[2].price: 'No data found'}<br>${data.pricing?data.pricing[2].plan: 'No data found'}</p>
-    `
-
-    const Integration = document.getElementById('Integration');
-    Integration.innerText =data ? data.integrations[0]: 'No data found';
-
-    const Integration1 = document.getElementById('Integration1');
-    Integration1.innerText = data ? data.integrations[1]: 'No data found';
-
-    const Integration2 = document.getElementById('Integration2');
-    Integration2.innerText =data ? data.integrations[2]: 'No data found';
+    modalContainers.appendChild(modalDiv);  
 
     const modalImg = document.getElementById('modal-img');
     modalImg.innerHTML = '';
@@ -134,3 +144,36 @@ document.getElementById('show-more').addEventListener('click', function(){
 loadAiData(6);
 
 //style="width: 18rem;
+
+//const modalTitle = document.getElementById('modal-description');
+    //modalTitle.innerText = data.description
+// const features = document.getElementById('Featurs');
+    // features.innerText = data.features ? data.features[1].feature_name : 'No data found';
+
+    // const features1 = document.getElementById('Featurs1');
+    // features1.innerText = data.features ? data.features[2].feature_name : 'No data found';
+
+    // const features2 = document.getElementById('Featurs2');
+    // features2.innerText = data.features ? data.features[3].feature_name : 'No data found';
+
+    // const priceContainer = document.getElementById('price1');
+    // priceContainer.innerHTML = `
+    // <p>${data.pricing? data.pricing[0].price : 'No data found'}<br>${data.pricing?data.pricing[0].plan : 'No data found'}</p>
+    // `
+    // const priceContainer1 = document.getElementById('price2');
+    // priceContainer1.innerHTML = `
+    // <p>${data.pricing?data.pricing[1].price:'No data found'}<br>${data.pricing?data.pricing[1].plan:'No data found'}</p>
+    // `
+    // const priceContainer2 = document.getElementById('contact');
+    // priceContainer2.innerHTML = `
+    // <p>${data.pricing?data.pricing[2].price: 'No data found'}<br>${data.pricing?data.pricing[2].plan: 'No data found'}</p>
+    // `
+
+    // const Integration = document.getElementById('Integration');
+    // Integration.innerText =data ? data.integrations[0]: 'No data found';
+
+    // const Integration1 = document.getElementById('Integration1');
+    // Integration1.innerText = data ? data.integrations[1]: 'No data found';
+
+    // const Integration2 = document.getElementById('Integration2');
+    // Integration2.innerText =data ? data.integrations[2]: 'No data found';
